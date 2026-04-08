88019058abf5d7ff38d09b48771aa513ad5a9bb0
sangdeptrai216 <chuhongsang2005@gmail.com>
2026-04-08 17:19:41 +0700

    update Product

--- Controller/Product.js
+++ Controller/Product.js
@@ -15,3 +15,30 @@ export const getProductsByCategory = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const getActiveProducts = async (req, res) => {
+    try {
+        const products = await Product.find({ status: 'ACTIVE' });
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const getUpcomingProducts = async (req, res) => {
+    try {
+        const products = await Product.find({ status: 'UPCOMING' });
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const getEndedProducts = async (req, res) => {
+    try {
+        const products = await Product.find({ status: 'ENDED' });
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
--- Router/Product.js
+++ Router/Product.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { getProductsByCategory } from "../Controller/Product.js";
+import { getActiveProducts } from "../Controller/Product.js";
+import { getUpcomingProducts} from "../Controller/Product.js";
+import { getEndedProducts } from "../Controller/Product.js";
 
 const router = express.Router();
 
@@ -12,4 +15,7 @@ router.get("/getbyid/:id", get(product));
 router.put("/update/:id", update(product));
 router.delete("/delete/:id", remove(product));
 router.get("/findByCategory", getProductsByCategory);
+router.get("/active", getActiveProducts);
+router.get("/upcoming", getUpcomingProducts);
+router.get("/ended", getEndedProducts);
 export default router;
